e92b33efc9433a7e2904b6fcacfaaa6aee9ee9b7
wellsjo <wellsjohnston@gmail.com>
2023-02-11 09:57:30 -0500

    able to generate nxn map with heightmap and terrain zones

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            Debug.Log("Generate button pressed");
+            mapGen.GenerateMap();
+        }
+    }
+}
